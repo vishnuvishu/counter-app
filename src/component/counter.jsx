7cefcd05d7237190383645d3ac0143ef653d5693
JPd iMac 1 <jpdimac1@JPd-iMac-1.local>
2019-06-19 11:04:26 +0400

    conditional rendering

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,35 +1,23 @@
 import React, { Component } from 'react';
 class Counter extends Component {
-    state =     {count: 1,
-    tags: ['tag1', 'tag2', 'tag3']}
-    // imageUrl: "https://picsum.photos/200"}
-    styles = {
-        fontSize: 20,
-        fontWeight: "bold"
-    };
+    state = {
+        count: 1,
+        tags: []
+    }
+
+    renderTags(){
+        if (this.state.tags.length === 0) return <p>No list found</p>
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
+
     render() { 
-      //  let classes = this.getBatchClasses();
         return (
-        <React.Fragment>
-            <span style={this.styles} className={this.getBatchClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            {this.state.tags.map((tag) => {
-                return <li key={tag}>{tag}</li>
-            })}
-        </React.Fragment>   
+        <div>  
+            {this.state.tags.length === 0 && 'please create a tag'}
+            {this.renderTags()}
+        </div>   
         );
-    }
-
-    getBatchClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCount () {
-        const { count } = this.state
-        return count === 0 ? 'Zero' : count;
     }
 }
  
-export default Counter;
+export default Counter; 
