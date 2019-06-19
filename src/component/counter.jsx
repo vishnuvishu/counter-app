db5479d208b6785a755d58f6c382c0cd45b9e991
JPd iMac 1 <jpdimac1@JPd-iMac-1.local>
2019-06-19 09:32:26 +0400

    rendering list

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 class Counter extends Component {
-    state =     {count: 0}
+    state =     {count: 1,
+    tags: ['tag1', 'tag2', 'tag3']}
     // imageUrl: "https://picsum.photos/200"}
     styles = {
-        fontSize: 50,
+        fontSize: 20,
         fontWeight: "bold"
     };
     render() { 
+      //  let classes = this.getBatchClasses();
         return (
         <React.Fragment>
-            <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+            <span style={this.styles} className={this.getBatchClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
-        </React.Fragment>
+            {this.state.tags.map((tag) => {
+                return <li key={tag}>{tag}</li>
+            })}
+        </React.Fragment>   
         );
+    }
+
+    getBatchClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount () {
